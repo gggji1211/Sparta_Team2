b1bcf729f45960af97326bfa3711aeed28ad9cc8
gggji1211 <c061211@naver.com>
2025-02-21 20:56:02 +0900

    보스 패턴

--- Assets/Script/BossAi.cs
+++ Assets/Script/BossAi.cs
@@ -1,37 +1,59 @@
+using System.Collections;
 using UnityEngine;
+
 
 public class BossAI : MonoBehaviour
 {
     public enum BossState { Idle, Chase }
+
     public BossState currentState = BossState.Idle;
 
     public Transform player;
+    private bool isEnraged = false;
     public float moveSpeed = 2f;
+
+    public float moveSpeedUp = 1.5f;
+
+    public float bossHp = 100f;
+
     public float detectionRange = 5f;
 
+    public float attackRange = 1.5f;
+
     private Rigidbody2D boss;
+
     private Vector2 movement;
 
     void Start()
-    {
+    { 
         boss = GetComponent<Rigidbody2D>();
+
     }
 
     void Update()
     {
         float distance = Vector2.Distance(transform.position, player.position);
 
+        CheckEnrage(bossHp);
+
         switch (currentState)
         {
-            case BossState.Idle:
-                if (distance < detectionRange)
+            case BossState.Idle: 
+                if (distance < detectionRange) 
                 {
-                    currentState = BossState.Chase;
+                    currentState = BossState.Chase; 
                 }
                 break;
 
             case BossState.Chase:
-                ChasePlayer();
+                if (distance <= attackRange)
+                {
+                    AttackPlayer();
+                }
+                else
+                {
+                    ChasePlayer(); 
+                }
                 break;
         }
     }
@@ -39,7 +61,30 @@ public class BossAI : MonoBehaviour
     void ChasePlayer()
     {
         Vector2 direction = (player.position - transform.position).normalized;
+
         movement = direction * moveSpeed;
+
         boss.velocity = movement;
     }
+
+    void AttackPlayer()
+    {
+        
+            Debug.Log("보스가 플레이어를 공격함!");
+         
+    }
+
+    void CheckEnrage(float currentHealth)
+    {
+        if ( isEnraged && bossHp <= 30)
+        {
+            isEnraged = true;
+            moveSpeed *= moveSpeedUp;
+            Debug.Log("보스분노 속도 증가");
+        }
+    }
 }
+
+
+
+
